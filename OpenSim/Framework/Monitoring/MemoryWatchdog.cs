ca207ef6c8951e52f7d2c629610183d9b85c91a3
BriakallistaStarfinder <briakallista@secondgalaxy.com>
2018-12-02 12:56:50 -0500

    Minor cleanup of the MemoryWatchdog
    Signed-Off-By: BriakallistaStarfinder<briakallista@secondgalaxy.com>

--- OpenSim/Framework/Monitoring/MemoryWatchdog.cs
+++ OpenSim/Framework/Monitoring/MemoryWatchdog.cs
@@ -1,29 +1,31 @@
-/*
- * Copyright (c) Contributors, http://opensimulator.org/
- * See CONTRIBUTORS.TXT for a full list of copyright holders.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the OpenSimulator Project nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+/// <license>
+///     Copyright (c) Contributors, http://virtual-planets.org/
+///     See CONTRIBUTORS.TXT for a full list of copyright holders.
+///     For an explanation of the license of each contributor and the content it
+///     covers please see the Licenses directory.
+///
+///     Redistribution and use in source and binary forms, with or without
+///     modification, are permitted provided that the following conditions are met:
+///         * Redistributions of source code must retain the above copyright
+///         notice, this list of conditions and the following disclaimer.
+///         * Redistributions in binary form must reproduce the above copyright
+///         notice, this list of conditions and the following disclaimer in the
+///         documentation and/or other materials provided with the distribution.
+///         * Neither the name of the Virtual Universe Project nor the
+///         names of its contributors may be used to endorse or promote products
+///         derived from this software without specific prior written permission.
+///
+///     THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+///     EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+///     WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+///     DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+///     DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+///     (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+///     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+///     ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+///     (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+///     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/// </license>
 
 using System;
 using System.Collections.Generic;
@@ -35,32 +37,31 @@ using log4net;
 namespace OpenSim.Framework.Monitoring
 {
     /// <summary>
-    /// Experimental watchdog for memory usage.
+    ///     Experimental watchdog for memory usage.
     /// </summary>
     public static class MemoryWatchdog
     {
-//        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-
         /// <summary>
-        /// Is this watchdog active?
+        ///     Is this watchdog active?
         /// </summary>
         public static bool Enabled
         {
             get { return m_enabled; }
             set
             {
-//                m_log.DebugFormat("[MEMORY WATCHDOG]: Setting MemoryWatchdog.Enabled to {0}", value);
-
                 if (value && !m_enabled)
+                {
                     UpdateLastRecord(GC.GetTotalMemory(false), Util.EnvironmentTickCount());
+                }
 
                 m_enabled = value;
             }
         }
+
         private static bool m_enabled;
 
         /// <summary>
-        /// Average heap allocation rate in bytes per millisecond.
+        ///     Average heap allocation rate in bytes per millisecond.
         /// </summary>
         public static double AverageHeapAllocationRate
         {
@@ -68,7 +69,7 @@ namespace OpenSim.Framework.Monitoring
         }
 
         /// <summary>
-        /// Last heap allocation in bytes
+        ///     Last heap allocation in bytes
         /// </summary>
         public static double LastHeapAllocationRate
         {
@@ -76,31 +77,26 @@ namespace OpenSim.Framework.Monitoring
         }
 
         /// <summary>
-        /// Maximum number of statistical samples.
+        ///     Maximum number of statistical samples.
         /// </summary>
         /// <remarks>
-        /// At the moment this corresponds to 1 minute since the sampling rate is every 2.5 seconds as triggered from
-        /// the main Watchdog.
+        ///     At the moment this corresponds to 1 minute since the sampling rate is every 2.5 seconds as triggered from
+        ///     the main Watchdog.
         /// </remarks>
         private static int m_maxSamples = 24;
 
         /// <summary>
-        /// Time when the watchdog was last updated.
+        ///     Time when the watchdog was last updated.
         /// </summary>
         private static int m_lastUpdateTick;
 
         /// <summary>
-        /// Memory used at time of last watchdog update.
+        ///     Memory used at time of last watchdog update.
         /// </summary>
         private static long m_lastUpdateMemory;
 
         /// <summary>
-        /// Memory churn rate per millisecond.
-        /// </summary>
-//        private static double m_churnRatePerMillisecond;
-
-        /// <summary>
-        /// Historical samples for calculating moving average.
+        ///     Historical samples for calculating moving average.
         /// </summary>
         private static Queue<double> m_samples = new Queue<double>(m_maxSamples);
 
@@ -110,20 +106,24 @@ namespace OpenSim.Framework.Monitoring
             long memoryNow = GC.GetTotalMemory(false);
             long memoryDiff = memoryNow - m_lastUpdateMemory;
 
-            if (memoryDiff >= 0)
+            if (m_samples.Count >= m_maxSamples)
             {
-                if (m_samples.Count >= m_maxSamples)
-                    m_samples.Dequeue();
-
-                double elapsed = Util.EnvironmentTickCountSubtract(now, m_lastUpdateTick);
-
-                // This should never happen since it's not useful for updates to occur with no time elapsed, but
-                // protect ourselves from a divide-by-zero just in case.
-                if (elapsed == 0)
-                    return;
-
-                m_samples.Enqueue(memoryDiff / (double)elapsed);
+                m_samples.Dequeue();
             }
+
+            double elapsed = Util.EnvironmentTickCountSubtract(now, m_lastUpdateTick);
+
+            /// <summary>
+            ///     This should never happen since it is not useful for updates
+            ///     to occur with no time elapsed, but lets protect ourselves from
+            ///     a divide-by-zero issue just in case.
+            /// </summary>
+            if (elapsed == 0)
+            {
+                return;
+            }
+
+            m_samples.Enqueue(memoryDiff / (double)elapsed);
 
             UpdateLastRecord(memoryNow, now);
         }
