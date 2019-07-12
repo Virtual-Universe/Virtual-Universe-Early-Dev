15540407844a345ee19600404e4efaf8fce9d09e
BruceStarfinder <bruce@secondgalaxy.com>
2019-07-12 14:49:20 -0400

    Add GodNamesModule
    - Add GodNamesModule
    - Fix license text to ensure security scan tools do not throw false errors
    - Remove commented out code to help prevent bloating of binary files
    - Cleanup comments
    - Cleanup whitespaces
    - Resharp files
    - Add configurable to Universe.ini.example for GodNamesModule
    
    Signed-Off-By: BruceStarfinder<bruce@secondgalaxy.com>

--- OpenSim/Region/CoreModules/Avatar/Gods/GodsModule.cs
+++ OpenSim/Region/CoreModules/Avatar/Gods/GodsModule.cs
@@ -1,29 +1,31 @@
-﻿/*
- * Copyright (c) Contributors, https://virtual-planets.org/
- * See CONTRIBUTORS.TXT for a full list of copyright holders.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Virtual Universe Project nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+﻿/// <license>
+///     Copyright (c) Contributors, https://virtual-planets.org/
+///     See CONTRIBUTORS.TXT for a full list of copyright holders.
+///     For an explanation of the license of each contributor and the content it
+///     covers please see the Licenses directory.
+///
+///     Redistribution and use in source and binary forms, with or without
+///     modification, are permitted provided that the following conditions are met:
+///         * Redistributions of source code must retain the above copyright
+///         notice, this list of conditions and the following disclaimer.
+///         * Redistributions in binary form must reproduce the above copyright
+///         notice, this list of conditions and the following disclaimer in the
+///         documentation and/or other materials provided with the distribution.
+///         * Neither the name of the Virtual Universe Project nor the
+///         names of its contributors may be used to endorse or promote products
+///         derived from this software without specific prior written permission.
+///
+///     THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+///     EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+///     WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+///     DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+///     DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+///     (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+///     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+///     ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+///     (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+///     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/// </license>
 
 using System;
 using System.Collections;
@@ -43,8 +45,8 @@ using OpenSim.Framework;
 using OpenSim.Framework.Capabilities;
 using OpenSim.Framework.Servers;
 using OpenSim.Framework.Servers.HttpServer;
-using OpenSim.Region.Framework.Scenes;
 using OpenSim.Region.Framework.Interfaces;
+using OpenSim.Region.Framework.Scene;
 using Caps = OpenSim.Framework.Capabilities.Caps;
 using OSDArray = OpenMetaverse.StructuredData.OSDArray;
 using OSDMap = OpenMetaverse.StructuredData.OSDMap;
@@ -54,10 +56,11 @@ namespace OpenSim.Region.CoreModules.Avatar.Gods
     [Extension(Path = "/OpenSim/RegionModules", NodeName = "RegionModule", Id = "GodsModule")]
     public class GodsModule : INonSharedRegionModule, IGodsModule
     {
-        private static readonly ILog m_log =
-            LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
-        /// <summary>Special UUID for actions that apply to all agents</summary>
+        /// <summary>
+        /// Special UUID for actions that apply to all agents
+        /// </summary>
         private static readonly UUID ALL_AGENTS = new UUID("44e87126-e794-4ded-05b3-7c42da3d5cdb");
 
         protected Scene m_scene;
@@ -67,7 +70,9 @@ namespace OpenSim.Region.CoreModules.Avatar.Gods
             get
             {
                 if (m_dialogModule == null)
+                {
                     m_dialogModule = m_scene.RequestModuleInterface<IDialogModule>();
+                }
 
                 return m_dialogModule;
             }
@@ -83,8 +88,7 @@ namespace OpenSim.Region.CoreModules.Avatar.Gods
             m_scene.RegisterModuleInterface<IGodsModule>(this);
             m_scene.EventManager.OnNewClient += SubscribeToClientEvents;
             m_scene.EventManager.OnRegisterCaps += OnRegisterCaps;
-            scene.EventManager.OnIncomingInstantMessage +=
-                    OnIncomingInstantMessage;
+            scene.EventManager.OnIncomingInstantMessage += OnIncomingInstantMessage;
         }
 
         public void RemoveRegion(Scene scene)
@@ -98,8 +102,12 @@ namespace OpenSim.Region.CoreModules.Avatar.Gods
         {
         }
 
-        public void Close() {}
-        public string Name { get { return "Gods Module"; } }
+        public void Close() { }
+
+        public string Name
+        {
+            get { return "Gods Module"; }
+        }
 
         public Type ReplaceableInterface
         {
@@ -111,25 +119,23 @@ namespace OpenSim.Region.CoreModules.Avatar.Gods
             client.OnGodKickUser += KickUser;
             client.OnRequestGodlikePowers += RequestGodlikePowers;
         }
-        
+
         public void UnsubscribeFromClientEvents(IClientAPI client)
         {
             client.OnGodKickUser -= KickUser;
             client.OnRequestGodlikePowers -= RequestGodlikePowers;
         }
-        
+
         private void OnRegisterCaps(UUID agentID, Caps caps)
         {
             string uri = "/CAPS/" + UUID.Random();
 
             caps.RegisterHandler(
-                "UntrustedSimulatorMessage", 
+                "UntrustedSimulatorMessage",
                 new RestStreamHandler("POST", uri, HandleUntrustedSimulatorMessage, "UntrustedSimulatorMessage", null));
         }
 
-        private string HandleUntrustedSimulatorMessage(string request,
-                string path, string param, IOSHttpRequest httpRequest,
-                IOSHttpResponse httpResponse)
+        private string HandleUntrustedSimulatorMessage(string request, string path, string param, IOSHttpRequest httpRequest, IOSHttpResponse httpResponse)
         {
             OSDMap osd = (OSDMap)OSDParser.DeserializeLLSDXml(request);
 
@@ -147,20 +153,23 @@ namespace OpenSim.Region.CoreModules.Avatar.Gods
                 uint kickFlags = userData["KickFlags"].AsUInteger();
                 string reason = userData["Reason"].AsString();
                 ScenePresence god = m_scene.GetScenePresence(godID);
+
                 if (god == null || god.ControllingClient.SessionId != godSessionID)
+                {
                     return String.Empty;
+                }
 
                 KickUser(godID, godSessionID, agentID, kickFlags, Util.StringToBytes1024(reason));
             }
             else
             {
-                m_log.ErrorFormat("[GOD]: Unhandled UntrustedSimulatorMessage: {0}", message);
+                m_log.ErrorFormat("[Gods]: Unhandled UntrustedSimulatorMessage: {0}", message);
             }
+
             return String.Empty;
         }
 
-        public void RequestGodlikePowers(
-            UUID agentID, UUID sessionID, UUID token, bool godLike, IClientAPI controllingClient)
+        public void RequestGodlikePowers(UUID agentID, UUID sessionID, UUID token, bool godLike, IClientAPI controllingClient)
         {
             ScenePresence sp = m_scene.GetScenePresence(agentID);
 
@@ -177,11 +186,11 @@ namespace OpenSim.Region.CoreModules.Avatar.Gods
                 {
                     // Next we check for spoofing.....
                     UUID testSessionID = sp.ControllingClient.SessionId;
+
                     if (sessionID == testSessionID)
                     {
                         if (sessionID == controllingClient.SessionId)
                         {
-                            //m_log.Info("godlike: " + godLike.ToString());
                             sp.GrantGodlikePowers(agentID, testSessionID, token, godLike);
                         }
                     }
@@ -189,11 +198,13 @@ namespace OpenSim.Region.CoreModules.Avatar.Gods
                 else
                 {
                     if (DialogModule != null)
+                    {
                         DialogModule.SendAlertToUser(agentID, "Request for god powers denied");
+                    }
                 }
             }
         }
-        
+
         /// <summary>
         /// Kicks User specified from the simulator. This logs them off of the grid
         /// If the client gets the UUID: 44e87126e7944ded05b37c42da3d5cdb it assumes
@@ -208,71 +219,78 @@ namespace OpenSim.Region.CoreModules.Avatar.Gods
         public void KickUser(UUID godID, UUID sessionID, UUID agentID, uint kickflags, byte[] reason)
         {
             if (!m_scene.Permissions.IsGod(godID))
+            {
                 return;
+            }
 
             ScenePresence sp = m_scene.GetScenePresence(agentID);
 
             if (sp == null && agentID != ALL_AGENTS)
             {
-                IMessageTransferModule transferModule =
-                        m_scene.RequestModuleInterface<IMessageTransferModule>();
+                IMessageTransferModule transferModule = m_scene.RequestModuleInterface<IMessageTransferModule>();
+
                 if (transferModule != null)
                 {
-                    m_log.DebugFormat("[GODS]: Sending nonlocal kill for agent {0}", agentID);
+                    m_log.DebugFormat("[Gods]: Sending nonlocal kill for agent {0}", agentID);
                     transferModule.SendInstantMessage(new GridInstantMessage(
                             m_scene, godID, "God", agentID, (byte)250, false,
                             Utils.BytesToString(reason), UUID.Zero, true,
-                            new Vector3(), new byte[] {(byte)kickflags}, true),
-                            delegate(bool success) {} );
+                            new Vector3(), new byte[] { (byte)kickflags }, true),
+                            delegate (bool success) { });
                 }
+
                 return;
             }
 
             switch (kickflags)
             {
-            case 0:
-                if (sp != null)
-                {
-                    KickPresence(sp, Utils.BytesToString(reason));
-                }
-                else if (agentID == ALL_AGENTS)
-                {
-                    m_scene.ForEachRootScenePresence(
-                            delegate(ScenePresence p)
-                            {
-                                if (p.UUID != godID && (!m_scene.Permissions.IsGod(p.UUID)))
-                                    KickPresence(p, Utils.BytesToString(reason));
-                            }
-                    );
-                }
-                break;
-            case 1:
-                if (sp != null)
-                {
-                    sp.AllowMovement = false;
-                    m_dialogModule.SendAlertToUser(agentID, Utils.BytesToString(reason));
-                    m_dialogModule.SendAlertToUser(godID, "User Frozen");
-                }
-                break;
-            case 2:
-                if (sp != null)
-                {
-                    sp.AllowMovement = true;
-                    m_dialogModule.SendAlertToUser(agentID, Utils.BytesToString(reason));
-                    m_dialogModule.SendAlertToUser(godID, "User Unfrozen");
-                }
-                break;
-            default:
-                break;
+                case 0:
+                    if (sp != null)
+                    {
+                        KickPresence(sp, Utils.BytesToString(reason));
+                    }
+                    else if (agentID == ALL_AGENTS)
+                    {
+                        m_scene.ForEachRootScenePresence(
+                                delegate (ScenePresence p)
+                                {
+                                    if (p.UUID != godID && (!m_scene.Permissions.IsGod(p.UUID)))
+                                    {
+                                        KickPresence(p, Utils.BytesToString(reason));
+                                    }
+                                });
+                    }
+                    break;
+                case 1:
+                    if (sp != null)
+                    {
+                        sp.AllowMovement = false;
+                        m_dialogModule.SendAlertToUser(agentID, Utils.BytesToString(reason));
+                        m_dialogModule.SendAlertToUser(godID, "User Frozen");
+                    }
+                    break;
+                case 2:
+                    if (sp != null)
+                    {
+                        sp.AllowMovement = true;
+                        m_dialogModule.SendAlertToUser(agentID, Utils.BytesToString(reason));
+                        m_dialogModule.SendAlertToUser(godID, "User Unfrozen");
+                    }
+                    break;
+                default:
+                    break;
             }
         }
 
         private void KickPresence(ScenePresence sp, string reason)
         {
             if (sp.IsChildAgent)
+            {
                 return;
+            }
+
             sp.ControllingClient.Kick(reason);
-            sp.Scene.CloseAgent(sp.UUID, true); 
+            sp.Scene.CloseAgent(sp.UUID, true);
         }
 
         private void OnIncomingInstantMessage(GridInstantMessage msg)
@@ -283,7 +301,6 @@ namespace OpenSim.Region.CoreModules.Avatar.Gods
                 string reason = msg.message;
                 UUID godID = new UUID(msg.fromAgentID);
                 uint kickMode = (uint)msg.binaryBucket[0];
-
                 KickUser(godID, UUID.Zero, agentID, kickMode, Util.StringToBytes1024(reason));
             }
         }
