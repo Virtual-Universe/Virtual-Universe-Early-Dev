ded75e6f1254c70fdcd3e1129adae463ab9fdfc0
EmperorStarfinder <emperor@secondgalaxy.com>
2019-09-18 07:24:12 -0400

    Restore previous way compile and build worked
    NOTE: We are restoring this as certian files were not in fact getting built causing issues when running the architecture.
    Signed-off-by: EmperorStarfinder <emperor@secondgalaxy.com>

--- runprebuild.bat
+++ runprebuild.bat
@@ -1,151 +1,27 @@
+@echo OFF
 
-@ECHO OFF
+Prebuild.exe /target vs2015
 
-echo ========================================
-echo ==== Virtual Universe Configuration ====
-echo ========================================
-echo.
-echo If you wish to customize the configuration, re-run with the switch '-p'
-echo   e.g.   runprebuild -p
-echo.
-
-rem ## Default "configuration" choice ((r)elease, (d)ebug)
-set configuration=d
-
-rem ## Default "run compile batch" choice (y(es),n(o))
-set compile_at_end=y
-
-rem ## Default Visual Studio edition
-set vstudio=2015
-
-rem ## Default Framework
-set framework=4_0
-
-rem ## Default architecture (86 (for 32bit), 64)
-:CheckArch
-set bits=x86
-if exist "%PROGRAMFILES(X86)%" (set bits=x64)
-if %bits% == x64 (
-	echo Found 64bit architecture
-    set args=/p:Platform=x64
-)
-if %bits% == x86 (
-	echo Found 32 bit architecture
-	set args=/p:Platform=x86
-)
-
-rem ## Determine native framework
-:CheckOS
-set framework=4_5
-if %framework%==4_5 set %vstudio%=2012
-
-for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
-if %version% == 10.0 (
-	set framework=4_6
-	echo Windows 10
-	rem ## As of April update 2018
-)
-if %version% == 6.3 (
-	set framework=4_5
-	echo Windows 8.1 or Server 2012 R2
-)
-if %version% == 6.2 (
-	set framework=4_5
-	echo Windows 8 or Server 2012
-)
-if %version% == 6.1 (
-	set framework=4_0
-	echo Windows 7 or Server 2008 R2
-)
-if %version% == 6.0 (
-	set framework=3_5
-	echo hmmm... Windows Vista or Server 2008
-)
-if %version% == 5.2 (
-	set framework=3_5
-	echo hmmm... Windows XP x64 or  Server 2003
-)
-if %version% == 5.1 (
-	set framework=3_5
-	echo hmmm... mutter, mutter Windows XP
-)
-
-rem ## If not requested, skip the prompting
-if "%1" =="" goto final
-if %1 == -p goto prompt
-if %1 == --prompt goto prompt
-goto final
-
-:prompt
-echo I will now ask you four questions regarding your build.
-echo However, if you wish to build for:
-echo		%bits% Architecture
-echo		.NET %framework%
-echo		Visual Studio %vstudio%
-if %compile_at_end% == y echo And you would like to compile straight after prebuild...
-echo.
-echo Simply tap [ENTER] three times.
-echo.
-echo Note that you can change these defaults by opening this
-echo batch file in a text editor.
-echo.
-
-:bits
-set /p bits="Choose your architecture (x86, x64) [%bits%]: "
-if %bits%==86 goto configuration
-if %bits%==x86 goto configuration
-if %bits%==64 goto configuration
-if %bits%==x64 goto configuration
-echo "%bits%" isn't a valid choice!
-goto bits
-
-:configuration
-set /p configuration="Choose your configuration ((r)elease or (d)ebug)? [%configuration%]: "
-if %configuration%==r goto framework
-if %configuration%==d goto framework
-if %configuration%==release goto framework
-if %configuration%==debug goto framework
-echo "%configuration%" isn't a valid choice!
-goto configuration
-
-:framework
-set /p framework="Choose your .NET framework (4.0, 4.5, 4.6, 4.7)? [%framework%]: "
-if %framework%==4.0 goto final
-if %framework%==4.5 goto final
-if %framework%==4.6 goto final
-if %framework%==4.7 goto final
-echo "%framework%" isn't a valid choice!
-goto framework
-
-:final
-
-if exist Compile.bat (
-    echo Deleting previous compile batch file...
-    echo.
-    del Compile.bat
-)
-
-
-echo.
-echo Setting up for build
-set FileName=Compile.bat
 setlocal ENABLEEXTENSIONS
 set VALUE_NAME=MSBuildToolsPath
-rem try find vs2019,20177
+
 if "%PROCESSOR_ARCHITECTURE%"=="x86" set PROGRAMS=%ProgramFiles%
 if defined ProgramFiles(x86) set PROGRAMS=%ProgramFiles(x86)%
 
+rem Try to find VS2019
 for %%e in (Enterprise Professional Community) do (
+    if exist "%PROGRAMS%\Microsoft Visual Studio\2019\%%e\MSBuild\Current\Bin\MSBuild.exe" (
 
-    if exist "%PROGRAMS%\Microsoft Visual Studio\2017\%%e\MSBuild\15.0\Bin\MSBuild.exe" (
-        set fpath="%PROGRAMS%\Microsoft Visual Studio\2017\%%e\MSBuild\15.0\Bin\"
-		rem set vstudio=2017
+        set ValueValue="%PROGRAMS%\Microsoft Visual Studio\2019\%%e\MSBuild\Current\Bin\MSBuild"
 		goto :found
     )
+)
 
-    if exist "%PROGRAMS%\Microsoft Visual Studio\2019\%%e\MSBuild\Current\Bin\MSBuild.exe" (
-        set fpath="%PROGRAMS%\Microsoft Visual Studio\2019\%%e\MSBuild\Current\Bin\"
-		rem set vstudio=2019
+rem try find vs2017
+for %%e in (Enterprise Professional Community) do (
+    if exist "%PROGRAMS%\Microsoft Visual Studio\2017\%%e\MSBuild\15.0\Bin\MSBuild.exe" (
+
+        set ValueValue="%PROGRAMS%\Microsoft Visual Studio\2017\%%e\MSBuild\15.0\Bin\MSBuild"
 		goto :found
     )
 )
@@ -164,48 +40,22 @@ if defined FOUNDGREP (
 
 rem try vs2015
 FOR /F "usebackq tokens=1-3" %%A IN (`REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\MSBuild\ToolsVersions\14.0" /v %VALUE_NAME% 2^>nul ^| %FINDCMD% "%VALUE_NAME%"`) DO (
-	set fpath=%%C
+	set ValueValue=%%C\msbuild
 	goto :found
 )
 
-echo msbuild for at least VS2015 not found, please install a (Community) edition of VS2019, VS2017 or VS2015
-echo Not creating %FileName%
-if exist %FileName% (
-	del %FileName%
+@echo msbuild for at least VS2015 not found, please install a (Community) edition of VS2017 or VS2015
+@echo Not creating compile.bat
+if exist "compile.bat" (
+	del compile.bat
 	)
 goto :done
 
 :found
-	echo Creating solution 
-	echo Calling Prebuild for target VS%vstudio% with framework %framework%...
-	Prebuild.exe /target vs%vstudio% /targetframework v%framework% /conditionals ISWIN;NET_%framework%
-	echo.
-    echo Found msbuild at %fpath%
-	echo.
-	echo Configuring for %bits% architecture using %framework% .NET framework
-	echo.
-	echo.
-	set /p nothing= Enter to continue
-
-    echo Creating build files
-
-	if %configuration%==r  (
-		set cfg=/p:Configuration=Release
-		set configuration=release
-	)
-	if %configuration%==d  (
-		set cfg=/p:Configuration=Debug
-		set configuration=debug
-	)
-	if %configuration%==release set cfg=/p:Configuration=Release
-	if %configuration%==debug set cfg=/p:Configuration=Debug
-
-	echo Creating %FileName%
-	echo %fpath%\msbuild Universe.sln %args% %cfg% > %FileName% /p:DefineConstants="ISWIN;NET_%framework%
-	
-	echo.
-	set /p compile_at_end="Done, %FileName% created. Compile now? (y,n) [%compile_at_end%]"
-	if %compile_at_end%==y (
-		%FileName%
-		pause
-)
+    @echo Found msbuild at %ValueValue%
+    @echo Creating compile.bat
+rem To compile in debug mode
+    @echo %ValueValue% universe.sln > compile.bat
+rem To compile in release mode comment line (add rem to start) above and uncomment next (remove rem)
+rem @echo %ValueValue% /P:Config=Release universe.sln > compile.bat
+:done
--- runprebuild.sh
+++ runprebuild.sh
@@ -1,107 +1,26 @@
-#!/bin/bash
-# Run prebuild to configure and create the appropriate Solution and Project files for building Virtual Universe Open Source Virtual World Architecture
-#
-# September 2019
-# Emperor Starfinder <emperor@secondgalaxy.com>
-#
-# Updated Dec 2018 to use NET 4.6 framework, msbuild (included in Mono V5+)
+#!/bin/sh
 
-ARCH="x64"
-CONFIG="Debug"
-BUILD=false
-VERSIONONLY=false
+case "$1" in
 
-USAGE="[-c <config>] -a <arch> -v"
-LONG_USAGE="Configuration options to pass to prebuild environment
-Options:
-  -c|--config Build configuration Debug (default) or Release
-  -a|--arch Architecture to target x86 or x64 (default)
-  -b|--build Build after configuration No (default) or Yes
-  -v|--version Update version details only
-"
-# get the current system architecture
-if (( 1 == 1<<32 )); then
-    ARCH="x86";
-    echo "x86 architecture detected";
-else
-    echo "x64 architecture found";
-fi
+  'clean')
 
-# check if prompting needed
-if [ $# -eq 0 ]; then
-    read -p "Architecture to use? (x86, x64) [$ARCH]: " bits
-    if [[ $bits == "x86" ]]; then ARCH="x86"; fi
-    if [[ $bits == "86" ]]; then ARCH="x86"; fi
-    if [[ $bits == "x64" ]]; then ARCH="x64"; fi
-    if [[ $bits == "64" ]]; then ARCH="x64"; fi
+    mono Prebuild.exe /clean
 
-    read -p "Configuration? (Release, Debug) [$CONFIG]: " conf
-    if [[ $conf == "Release" ]]; then CONFIG="Release"; fi
-    if [[ $conf == "release" ]]; then CONFIG="Release"; fi
+  ;;
 
-	bld="No"
-	if [[ $BUILD == true ]]; then bld="Yes"; fi
 
-    read -p "Build immediately? (Yes, No) [$bld]: " bld
-    if [[ $bld == "Yes" ]]; then BUILD=true; fi
-    if [[ $bld == "yes" ]]; then BUILD=true; fi
-    if [[ $bld == "y" ]]; then BUILD=true; fi
+  'autoclean')
 
-else
+    echo y|mono Prebuild.exe /clean
 
-# command line params supplied
-while case "$#" in 0) break ;; esac
-do
-  case "$1" in
-    -c|--config)
-      shift
-      CONFIG="$1"
-      ;;
-    -a|--arch)
-      shift
-      ARCH="$1"
-      ;;
-    -b|--build)
-      BUILD=true
-      shift
-      ;;
-    -v|--version)
-      VERSIONONLY=true
-      ;;
-    -h|--help)
-      echo "$USAGE"
-      echo "$LONG_USAGE"
-      exit
-      ;;
-    *)
-      echo "Illegal option!"
-      echo "$USAGE"
-      echo "$LONG_USAGE"
-      exit
-      ;;
-  esac
-  shift
-done
+  ;;
 
-fi
 
-# Configuring Virtual Universe
-if ! ${VERSIONONLY:=true}; then
-  echo "Configuring Virtual Universe $ARCH $CONFIG build"
-  mono ./Prebuild.exe /target vs2015 /targetframework v4_6 /conditionals "LINUX"
-fi
+  *)
 
-# Build Virtual Universe
-if ${BUILD:=true} ; then
-  echo Building Virtual Universe Open Source Virtual World Architecture
-  msbuild  Universe.sln /property:Configuration="$CONFIG" /property:Platform="$ARCH"
-  echo Finished Building Virtual Universe Open Source Virtual World Architecture
-  echo Thank you for choosing Virtual Universe Open Source Virtual World Architecture
-  echo Please report any errors to our Github Issue Tracker 
-  echo for Our Early Development Code: https://github.com/Virtual-Universe/Virtual-Universe-Early-Dev/issues
-  echo For our Stable Development Code: https://github.com/Virtual-Universe/Virtual-Universe-Stable-Dev/issues
-  echo For our Official Release Code: https://github.com/Virtual-Universe/Virtual-Universe-Release/issues
-else
-  echo "The Virtual Universe Open Source Virtual World Architecture has been configured to compile with $ARCH $CONFIG options"
-  echo "To manually build, enter 'msbuild Universe.sln' at the command prompt"
-fi
+    mono Prebuild.exe /target nant
+    mono Prebuild.exe /target vs2015
+
+  ;;
+
+esac
