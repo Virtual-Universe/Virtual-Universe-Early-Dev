3ade804411730d707c0cb6e9aef3475ec5970473
EmperorStarfinder <emperor@secondgalaxy.com>
2019-09-22 10:32:00 -0400

    [UPSTREAM]: Make Console.Output and Console.Prompt compatible with the rest of the code
    Signed-off-by: EmperorStarfinder <emperor@secondgalaxy.com>

--- Universe/Framework/Console/ConsoleBase.cs
+++ Universe/Framework/Console/ConsoleBase.cs
@@ -1,29 +1,31 @@
-/*
- * Copyright (c) Contributors, http://virtual-planets.org/
- * See CONTRIBUTORS.TXT for a full list of copyright holders.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Virtual Universe Project nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+/// <license>
+///     Copyright (c) Contributors, https://virtual-planets.org/
+///     See CONTRIBUTORS.TXT for a full list of copyright holders.
+///     For an explanation of the license of each contributor and the content it
+///     covers please see the Licenses directory.
+///
+///     Redistribution and use in source and binary forms, with or without
+///     modification, are permitted provided that the following conditions are met:
+///         * Redistributions of source code must retain the above copyright
+///         notice, this list of conditions and the following disclaimer.
+///         * Redistributions in binary form must reproduce the above copyright
+///         notice, this list of conditions and the following disclaimer in the
+///         documentation and/or other materials provided with the distribution.
+///         * Neither the name of the Virtual Universe Project nor the
+///         names of its contributors may be used to endorse or promote products
+///         derived from this software without specific prior written permission.
+///
+///     THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+///     EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+///     WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+///     DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+///     DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+///     (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+///     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+///     ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+///     (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+///     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/// </license>
 
 using System;
 using System.Collections.Generic;
@@ -37,8 +39,6 @@ namespace Universe.Framework.Console
 {
     public class ConsoleBase : IConsole
     {
-//        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-
         protected string prompt = "# ";
 
         public IScene ConsoleScene { get; set; }
@@ -58,23 +58,48 @@ namespace Universe.Framework.Console
         {
         }
 
-        public virtual void Output(string format, string level = null, params object[] components)
+        public void Output(string format, params object[] components)
+        {
+            Output(format, null, components);
+        }
+
+        public virtual void Output(string format, string level, params object[] components)
         {
             System.Console.WriteLine(format, components);
         }
 
-        public virtual string Prompt(string p, string def = null, List<char> excludedCharacters = null, bool echo = true)
+        public string Prompt(string p)
+        {
+            return Prompt(p, null, null, true);
+        }
+
+        public string Prompt(string p, string def)
+        {
+            return Prompt(p, def, null, true);
+        }
+
+        public string Prompt(string p, List<char> excludedCharacters)
+        {
+            return Prompt(p, null, excludedCharacters, true);
+        }
+
+        public virtual string Prompt(string p, string def, List<char> excludedCharacters, bool echo = true)
         {
             bool itisdone = false;
             string ret = String.Empty;
+
             while (!itisdone)
             {
                 itisdone = true;
 
                 if (def != null)
+                {
                     ret = ReadLine(String.Format("{0}: ", p), false, echo);
+                }
                 else
+                {
                     ret = ReadLine(String.Format("{0} [{1}]: ", p, def), false, echo);
+                }
 
                 if (ret == String.Empty && def != null)
                 {
@@ -99,15 +124,27 @@ namespace Universe.Framework.Console
             return ret;
         }
 
-        // Displays a command prompt and returns a default value, user may only enter 1 of 2 options
+        /// <summary>
+        /// Displays a command prompt and returns
+        /// a default value, user may only enter 1 or 
+        /// 2 options
+        /// </summary>
+        /// <param name="prompt"></param>
+        /// <param name="defaultresponse"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
         public virtual string Prompt(string prompt, string defaultresponse, List<string> options)
         {
             bool itisdone = false;
             string optstr = String.Empty;
+
             foreach (string s in options)
+            {
                 optstr += " " + s;
+            }
 
             string temp = Prompt(prompt, defaultresponse);
+
             while (itisdone == false)
             {
                 if (options.Contains(temp))
@@ -120,6 +157,7 @@ namespace Universe.Framework.Console
                     temp = Prompt(prompt, defaultresponse);
                 }
             }
+
             return temp;
         }
 
@@ -127,7 +165,6 @@ namespace Universe.Framework.Console
         {
             System.Console.Write("{0}", p);
             string cmdinput = System.Console.ReadLine();
-
             return cmdinput;
         }
     }
--- Universe/Framework/Console/MockConsole.cs
+++ Universe/Framework/Console/MockConsole.cs
@@ -1,29 +1,31 @@
-/*
- * Copyright (c) Contributors, http://virtual-planets.org/
- * See CONTRIBUTORS.TXT for a full list of copyright holders.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Virtual Universe Project nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+/// <license>
+///     Copyright (c) Contributors, https://virtual-planets.org/
+///     See CONTRIBUTORS.TXT for a full list of copyright holders.
+///     For an explanation of the license of each contributor and the content it
+///     covers please see the Licenses directory.
+///
+///     Redistribution and use in source and binary forms, with or without
+///     modification, are permitted provided that the following conditions are met:
+///         * Redistributions of source code must retain the above copyright
+///         notice, this list of conditions and the following disclaimer.
+///         * Redistributions in binary form must reproduce the above copyright
+///         notice, this list of conditions and the following disclaimer in the
+///         documentation and/or other materials provided with the distribution.
+///         * Neither the name of the Virtual Universe Project nor the
+///         names of its contributors may be used to endorse or promote products
+///         derived from this software without specific prior written permission.
+///
+///     THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+///     EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+///     WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+///     DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+///     DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+///     (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+///     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+///     ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+///     (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+///     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/// </license>
 
 using System;
 using System.Threading;
@@ -35,8 +37,10 @@ using Nini.Config;
 namespace Universe.Framework.Console
 {
     /// <summary>
-    /// This is a Fake console that's used when setting up the Scene in Unit Tests
-    /// Don't use this except for Unit Testing or you're in for a world of hurt when the
+    /// This is a Fake console that's used 
+    /// when setting up the Scene in Unit Tests
+    /// Don't use this except for Unit Testing 
+    /// or you're in for a world of hurt when the
     /// sim gets to ReadLine
     /// </summary>
     public class MockConsole : ICommandConsole
@@ -47,7 +51,10 @@ namespace Universe.Framework.Console
 
         private MockCommands m_commands = new MockCommands();
 
-        public ICommands Commands { get { return m_commands; } }
+        public ICommands Commands
+        {
+            get { return m_commands; }
+        }
 
         public string DefaultPrompt { get; set; }
 
@@ -57,14 +64,18 @@ namespace Universe.Framework.Console
 
         public string ReadLine(string p, bool isCommand, bool e) { return ""; }
 
-        public IScene ConsoleScene {
+        public IScene ConsoleScene
+        {
             get { return null; }
             set {}
         }
 
-        public void Output(string format, string level, params object[] components) {}
+        public void Output(string format, params object[] components) { }
+        public void Output(string format, string level, params object[] components) { }
 
         public string Prompt(string p) { return ""; }
+        public string Prompt(string p, string def) { return ""; }
+        public string Prompt(string p, List<char> excludedCharacters) { return ""; }
         public string Prompt(string p, string def, List<char> excludedCharacters, bool echo) { return ""; }
 
         public string Prompt(string prompt, string defaultresponse, List<string> options) { return ""; }
