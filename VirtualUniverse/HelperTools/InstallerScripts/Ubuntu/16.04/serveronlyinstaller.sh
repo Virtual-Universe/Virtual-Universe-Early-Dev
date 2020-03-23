3b4b8f7541c636ad37e7e4abb95840acaf3243dc
IrishwolfStarfinder <irishwolf@secondgalaxy.com>
2020-03-23 18:16:06 -0400

    Begin adding server installer scripts
    - Add serveronlyinstaller.sh scripts for Debian and Ubuntu
    
    NOTE: These installer scripts assume you have either Ubuntu Server or Debian Server installed without the desktop environment.  These scripts will automatically add the apt package repositories and will update and install all necessary packages on the server including attempting to pull the latest from the repository and build.
    
    NOTE 2: You will get errors during the build process for the Virtual-Universe-Early-Dev repository at the moment.  Do not panic about that.
    
    Signed-off-by: IrishwolfStarfinder <irishwolf@secondgalaxy.com>

---
+++ VirtualUniverse/HelperTools/InstallerScripts/Ubuntu/16.04/serveronlyinstaller.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Do the distribution upgrades
+sudo apt-get dist-upgrade -y
+# Add package repositories for mono-project
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu stable-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
+# Install Mono related packages
+sudo apt-get install mono-complete -y
+sudo apt-get install mono-xsp4 -y
+# Install MariaDB Server
+sudo apt-get install mariadb-server
+# Install Git
+sudo apt-get install git
+# Install Virtual Universe
+cd /
+mkdir Github
+cd Github/
+git clone --recursive https://github.com/Virtual-Universe/Virtual-Universe-Early-Dev.git
+cd Virtual-Universe-Early-Dev/
+./runprebuild.sh
+msbuild
